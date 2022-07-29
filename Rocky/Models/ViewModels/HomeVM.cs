a0f8abadc5fdc5fca820f5adc2812572a39ddc4f
vassilyev <vassilyev@toowto.kz>
2022-07-29 17:55:02 +0600

    Section5  after 3 part. Start implement home page

---
+++ Rocky/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Rocky.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+    }
+}
